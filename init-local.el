937cd85a966f8e104141ce95ab5ef2df0c611e1a
Erick Johnson <ejohnson82@gmail.com>
2012-05-07 17:06:14 -0400

    finally got emacsclient setting up frame fonts consistently

--- init-local.el
+++ init-local.el
@@ -1,1 +1,7 @@
+(defun setup-frame-local (frame)
+  "This function will setup my frames with the correct parameters since emacsclient does not want to do this automatically"
+  (set-frame-parameter frame 'font "Inconsolata-10"))
+
+(add-hook 'after-make-frame-functions 'setup-frame-local)
+
 (provide 'init-local)
