395ffea3175d9b44a1494cee6b6dfae356028476
Erick Johnson <erick@ejjohnson.org>
2013-03-16 23:34:26 -0400

    can't remember what I was trying to do, but this sets the emacs PATH env correctly from my enviroment, YMMV

--- init-exec-path.el
+++ init-exec-path.el
@@ -3,11 +3,12 @@
 
 This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
   (interactive)
-  (let ((path-from-shell (string-rtrim (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+  (let ((path-from-shell (string-rtrim
+                          (shell-command-to-string "$SHELL -i -c 'echo $PATH' 2> /dev/null"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
-(when (and *is-a-mac* window-system)
+(when (window-system)
   (set-exec-path-from-shell-PATH))
 
 
